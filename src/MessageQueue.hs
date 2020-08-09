1cc027114cc35cd129ba223873832c0bf5063a1a
Ik Mac <azarintrets@live.nl>
2020-08-09 02:54:49 +0200

    message limit size

--- src/MessageQueue.hs
+++ src/MessageQueue.hs
@@ -11,13 +11,14 @@ import Control.Monad.Reader
 import Control.Concurrent.STM.TBQueue
 import Control.Concurrent.STM
 import Bot.OnlineChecker
+import qualified Data.Text as T
 
 messageDispensingLoopM2 :: ConcM App ()
 messageDispensingLoopM2 = do
   taskM $ threadDelay 2000000
   (msgQ   , online) <- pureM $ (,) <$> asks messageQueue <*> isOnline
   (message, sem   ) <- taskM $ atomically $ readTBQueue msgQ
-  pureM $ do
+  when (T.length message < 500) $ pureM $ do
     privmsg message
     --unless online $ privmsg message
     liftIO $ putMVar sem True
