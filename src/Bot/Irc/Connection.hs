0e5698e1e0e23e844c69f74fcf6b62ed325c76d2
Ik Mac <azarintrets@live.nl>
2020-08-10 15:48:12 +0200

    program exits if fails to connect to irc

--- src/Bot/Irc/Connection.hs
+++ src/Bot/Irc/Connection.hs
@@ -1,28 +1,31 @@
 module Bot.Irc.Connection where
 
 import qualified Network.Socket as N
-import Bot
-import GHC.IO.Handle (Handle)
-import Control.Exception -- base
-import System.IO
+import           Bot
+import           GHC.IO.Handle (Handle)
+import           Control.Exception -- base
+import           System.IO
+import           System.Exit
 
 connect :: String -> N.PortNumber -> IO Bot
-connect server port = notify $ do
-  h <- connectTo server port
-  return (Bot h)
- where
-  notify a = bracket_
-    (putStrLn ("Connecting to " ++ server ++ " ...") >> hFlush stdout)
-    (putStrLn "done.")
-    a
+connect server port = notify
+  $ do
+    h <- connectTo server port
+    return (Bot h)
+  where
+    notify a = bracket_
+      (putStrLn ("Connecting to " ++ server ++ " ...") >> hFlush stdout)
+      (putStrLn "done.")
+      a
 
 -- Connect to the server and return a Handle (helper for connect)
 connectTo :: N.HostName -> N.PortNumber -> IO Handle
-connectTo host port = do
-  addr : _ <- N.getAddrInfo Nothing (Just host) (Just (show port))
-  sock     <- N.socket
-    (N.addrFamily addr)
-    (N.addrSocketType addr)
-    (N.addrProtocol addr)
-  N.connect sock (N.addrAddress addr)
-  N.socketToHandle sock ReadWriteMode
+connectTo host port = handle (\NonTermination -> exitWith (ExitFailure 1))
+  $ do
+    addr:_ <- N.getAddrInfo Nothing (Just host) (Just (show port))
+    sock <- N.socket
+      (N.addrFamily addr)
+      (N.addrSocketType addr)
+      (N.addrProtocol addr)
+    N.connect sock (N.addrAddress addr)
+    N.socketToHandle sock ReadWriteMode
